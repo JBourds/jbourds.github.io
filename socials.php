16f6fdf88aaad834bc85f2e23a7222c28c68fd7a
JBourds <jordanbourdeau03@outlook.com>
2022-10-02 21:15:02 -0400

    Minor CSS tweaks

--- socials.php
+++ socials.php
@@ -1,20 +1,20 @@
 <section class="socials">
     <h2>Follow me on Social Media!</h2>
-    <a href="https://www.linkedin.com/in/jordan-bourdeau-b4a624252/" class="center social-link">
+    <a href="https://www.linkedin.com/in/jordan-bourdeau-b4a624252/" target = "_blank" class="center social-link">
         <p>LinkedIn</p>
         <figure>
             <img class = "icon" src="<?php print $directoryPrefix; ?>/images/linkedIn.png">
         </figure>
     </a>
 
-    <a href="https://www.instagram.com/jordan_bourdeau/?hl=en" class="center social-link">
+    <a href="https://www.instagram.com/jordan_bourdeau/?hl=en" target = "_blank" class="center social-link">
         <p>Instagram</p>
         <figure>
             <img class = "icon" src="<?php print $directoryPrefix; ?>/images/instagram.jpg">
         </figure>
     </a>
 
-    <a href="https://www.facebook.com/jordan.bourdeau.5" class="center social-link">
+    <a href="https://www.facebook.com/jordan.bourdeau.5" target = "_blank" class="center social-link">
         <p>Facebook</p>
         <figure>
             <img class = "icon" src="<?php print $directoryPrefix; ?>/images/facebook.png">
