c9bb041bdfcf2abef47c0671068619d0821df4ad
JBourds <jordanbourdeau03@outlook.com>
2022-10-02 20:42:55 -0400

    Changed CSS so it dynamically sets the CSS based on the page

--- header.php
+++ header.php
@@ -2,25 +2,38 @@
     <p class="arrow down"></p>
 </a>
 
-<header class="paper-background">
 
-    <div class="toprow header-grid-layout">
+    <?php
+    if($fileName == "index") {
+        print '
+        <header class="paper-background">
+        <div class="toprow header-grid-layout">
         <p class="left-side card">802.782.3288</p>
         <p class="right-side-top card">
             <span class="align-right">University of Vermont</span>
         </p>
         <p class="right-side-bottom card">Computer and Data Science Major</p>
-    </div>
+        </div>
 
-    <div class="middlerow">
-        <h1 class="title card">Jordan Bourdeau</h1>
-        <p class="card">Programmer</p>
-    </div>
+        <div class="middlerow">
+            <h1 class="title card">Jordan Bourdeau</h1>
+            <p class="card">Programmer</p>
+        </div>
 
-    <div class="bottomrow">
-        <a href="contact.html" class="right-side card">Contact:
-            <span class="bold">Phone Email Social Media</span>
-        </a>
-    </div>
+        <div class="bottomrow">
+            <a href="contact.html" class="right-side card">Contact:
+                <span class="bold">Phone Email Social Media</span>
+            </a>
+        </div>
+        ' . PHP_EOL;
+    }
+
+    else {
+        print '<header class="universal-header-text">
+        <h1 class="header">Jordan Bourdeau Portfolio</h1>' . PHP_EOL;
+    }
+    ?>
+
+    
 
 </header>
--- top.php
+++ top.php
@@ -1,3 +1,38 @@
+<?php
+include 'lib/constants.php';
+$directoryDepth = 0; 
+    $domainLength = strlen(DOMAIN);
+    $labPrefix = '';
+    // Loop through every character in the path after the domain to determine the depth of the file
+    for ($index = $domainLength + 1; $index < strlen(BASE_PATH); $index++) { // + 1 ignores the backslash after the domain
+        // Since this comes directly after the domain, it will pull the first two directories after the domain <cs148/<lab>/
+        if ($directoryDepth < 2) {
+            $labPrefix .= BASE_PATH[$index];
+        }
+        if (BASE_PATH[$index] == '/') {
+            $directoryDepth++;
+        }
+        
+    }
+    // Creates the prefix to put in front of relative links based on how far down in the directory it is in order to bring user to the www-root/ directory
+    $directoryPrefix = '';
+    for ($step = 0; $step < $directoryDepth; $step++) {
+        $directoryPrefix .= '../';
+    }
+
+    // Pulls the folder and lab directory out of the base path variable and adds it to the directory prefix.
+    $directoryPrefix .= $labPrefix;
+
+    // Additional debugging statement
+    if (DEBUG) {
+        print '<p>' . $directoryPrefix . '</p>';
+    }
+
+    // Get the file name to display the correct css page
+    $fileName = PATH_PARTS['filename'];
+
+?>
+
 <!DOCTYPE html)>
 <html>
 
@@ -8,17 +43,16 @@
     <title>Jordan Bourdeau Portfolio</title>
     <meta name="Author" content="Jordan Bourdeau">
     <meta name="Description" content="Jordan Bourdeau Programming Portfolio">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/index_styles.css">
-    <link rel="stylesheet" type="text/css" media="(max-width: 600px)" href="css/phone.css">
-    <link rel="stylesheet" type="text/css" media="(max-width: 1100px)" href="css/tablet.css">
-    <link rel="stylesheet" type="text/css" media="(min-width: 3000px)" href="css/ultrawide.css">
+    <link rel="stylesheet" type="text/css" href="<?php print $directoryPrefix; ?>css/main.css?version=<?php print time(); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php print $directoryPrefix; ?>css/<?php print $fileName; ?>.css?version=<?php print time(); ?>">
+    <link rel="stylesheet" type="text/css" media="(max-width: 600px)" href="<?php print $directoryPrefix; ?>css/phone.css?version=<?php print time(); ?>">
+    <link rel="stylesheet" type="text/css" media="(max-width: 1100px)" href="<?php print $directoryPrefix; ?>css/tablet.css?version=<?php print time(); ?>">
+    <link rel="stylesheet" type="text/css" media="(min-width: 3000px)" href="<?php print $directoryPrefix; ?>css/ultrawide.css?version=<?php print time(); ?>">
 </head>
 
 <body>
 
 <?php
-include 'lib/constants.php';
 include 'header.php';
 include 'nav.php';
 ?>
